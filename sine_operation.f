993dcaf180a5017bab2c08a4bb933838f32f2ff0
Werner Krauth <werner.krauth@ens.fr>
2012-03-30 13:55:36 +0200

    minor update, fortran file updated

--- sine_operation.f
+++ sine_operation.f
@@ -3,12 +3,12 @@ cc PROGRAM : event_chain.f
 cc TYPE    : Fortran 77 program 
 cc PURPOSE : test program for f2py - multiprocessing
 cc COMMENT : 
-cc VERSION : 16 MAR 2012
+cc VERSION : 30 MAR 2012
 cc=======+=========+=========+=========+=========+=========+=========+==
       subroutine sine_add_1(data)
       real*8 data
 Cf2py intent (in,out) data
-      do k = 2, 100000
+      do k = 2, 1000000
          data = data + sin(1./k)
       end do
       end
@@ -16,8 +16,8 @@ Cf2py intent (in,out) data
       subroutine sine_add_2(index,data,N)
       real*8 data(N)
       integer index
-Cf2py intent (in,out) data
-      do k = 2, 100000
+Cf2py intent (inout) data
+      do k = 2, 1000000
          data(index+1) = data(index+1) + sin(1./k)
       end do
       end
